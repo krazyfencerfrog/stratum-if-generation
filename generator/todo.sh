155f0d16e1d4155c1817c3a79d2e9e6346720546
Krazy <krazyfencer@gmail.com>
2026-09-14 18:34:00 -0400

    proved improvement to 3_0a. possible improvement to 3_0b. fixed up todo script

--- generator/todo.sh
+++ generator/todo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-python main.py --story-id=kernel25 < ../tests/kernels/kernel25.txt
-python main.py --story-id=kernel26 < ../tests/kernels/kernel26.txt
 
 python main.py --story-id=kernel1 < ../tests/kernels/kernel1.txt
 python main.py --story-id=kernel2 < ../tests/kernels/kernel2.txt
@@ -26,3 +24,7 @@ python main.py --story-id=kernel20 < ../tests/kernels/kernel20.txt
 python main.py --story-id=kernel21 < ../tests/kernels/kernel21.txt
 python main.py --story-id=kernel22 < ../tests/kernels/kernel22.txt
 python main.py --story-id=kernel23 < ../tests/kernels/kernel23.txt
+python main.py --story-id=kernel24 < ../tests/kernels/kernel24.txt
+python main.py --story-id=kernel25 < ../tests/kernels/kernel25.txt
+python main.py --story-id=kernel26 < ../tests/kernels/kernel26.txt
+python main.py --story-id=kernel27 < ../tests/kernels/kernel27.txt
